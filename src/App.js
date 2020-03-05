b6658862a501534df43485937396ad615167a1f5
Mauri <mvergaracid@gmail.com>
2020-03-04 23:31:34 -0300

    super simple tutorial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect} from 'react';
+import './App.css'
 
 function App() {
+
+  const [ catUrl, setCatUrl] = useState('https://cdn2.thecatapi.com/images/9ch.jpg')
+
+  const apiKey = '4418b721-1550-491a-8387-edd601a73e2b'
+  const url = 'https://api.thecatapi.com/v1/images/search'
+
+  useEffect(() => {
+    getCat()
+  }, [])
+  
+  const getCat = async () => {
+    try {
+      const response = await fetch(url)
+      const json = await response.json()
+      if (json[0]) {
+        const catUrl = json[0].url
+        setCatUrl(catUrl)
+      }
+      console.log('cats:', json)
+    } catch(error) {
+      console.log('error:', error)
+    }
+  }
+
+  //JSX
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1> The best cap app ever created</h1>
+      <img src={catUrl} alt="miau" />
+      <button onClick={getCat}>Get new random cat</button>
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App
+
